f8f3a557d71bdb564625f3318780f4dc7365393c
shamim159 <shamin_ali@hotmail.co.uk>
2024-02-02 10:14:11 +0000

    Create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userShema = new mongoose.Schema({
+    username: {
+        type: String, 
+        required: true, //will not work without username
+        unique: true, //will not allow different users to have the same username
+    },
+    email: {
+        type: String, 
+        required: true, //will not work without email
+        unique: true, //will not allow different users to have the same username
+    },
+    password: {
+        type: String, 
+        required: true, //will not work without password
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', userSchema);
+
+export default User; //use the model anywhere else in the application!!
