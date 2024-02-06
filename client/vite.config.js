c9561e075031608745249edb082f20ed5224c611
shamim159 <shamin_ali@hotmail.co.uk>
2024-02-06 20:42:42 +0000

    sign up page funtionality complete

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {//everytime a request is made with /api it will add localhost:3000 at the beginning
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+});
