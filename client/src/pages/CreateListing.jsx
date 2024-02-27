a7a71285092bceee3f3b106e3a683e91cfe5cfca
shamim159 <shamin_ali@hotmail.co.uk>
2024-02-27 17:43:46 +0000

    create get user listings api route

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -141,7 +141,7 @@ export default function CreateListing() {
       if (data.success === false) {
         setError(data.message);
       }
-      navigate(`/Listing/${data._id}`)
+      navigate(`/listing/${data._id}`)
     } catch (error) {
       setError(error.message);
       setLoading(false);
