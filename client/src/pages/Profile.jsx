d22dce914b9f38736ad9ecd0749902050e361973
shamim159 <shamin_ali@hotmail.co.uk>
2024-02-27 22:12:18 +0000

    complete show listings link and functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -15,6 +15,8 @@ export default function Profile() {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingError, setShowListingError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
   //firebase storage: service firebase.storage 
   //    allow read;
@@ -108,6 +110,20 @@ const handleSignOut = async () => {
   }
 }
 
+  const handleShowListing = async () => {
+    try {
+      setShowListingError(false);
+      const res = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await res.json();
+      if (data.success === false) {
+        setShowListingError(true);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingError(true);
+    }
+  }
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
@@ -137,22 +153,73 @@ const handleSignOut = async () => {
               ''
           )}
         </p>
-        <input type="text" placeholder='username' defaultValue={currentUser.username} id='username' className='border p-3 rounded-lg' onChange={handleChange}/>
-        <input type="email" placeholder='email' defaultValue={currentUser.email} id='email' className='border p-3 rounded-lg' onChange={handleChange}/>
-        <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg' onChange={handleChange}/>
-        <button disabled={loading} className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}</button>
-        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' to={"/create-listing"}>
+        <input type="text" 
+          placeholder='username' 
+          defaultValue={currentUser.username} 
+          id='username' className='border p-3 rounded-lg' 
+          onChange={handleChange}
+        />
+        <input 
+          type="email" 
+          placeholder='email' 
+          defaultValue={currentUser.email} 
+          id='email' className='border p-3 rounded-lg' 
+          onChange={handleChange}
+        />
+        <input type="password" 
+          placeholder='password' 
+          id='password' 
+          className='border p-3 rounded-lg' 
+          onChange={handleChange}
+        />
+        <button disabled={loading} 
+          className='bg-slate-700 text-white rounded-lg p-3 uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Update'}
+        </button>
+        <Link className='bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95' 
+          to={"/create-listing"}>
           Create Listing
         </Link>
       </form>
       <div className="flex justify-between mt-5">
-        <span onClick={handleDeleteUser} className='cursor-pointer text-red-700'>Delete Account</span>
-        <span onClick={handleSignOut} className='text-red-700 cursor-pointer'>Sign out</span>
+        <span onClick={handleDeleteUser} 
+          className='cursor-pointer text-red-700'>Delete Account
+        </span>
+        <span 
+          onClick={handleSignOut} 
+          className='text-red-700 cursor-pointer'>Sign out
+        </span>
       </div>
       <p className='text-red-700 mt-5' >{error ? error : ''}</p>
       <p className='text-green-700 mt-5' >{updateSuccess ? 'User is updated Successfully!' : ''}</p>
+      <button 
+        onClick={handleShowListing} 
+        className='text-green-700 w-full' 
+        >Show Listings
+      </button>
+      <p className='text-red-700'>{showListingError ? 'Error showing listings' : '' }</p>
+      {userListings && userListings.length > 0 &&
+      <div className='flex flex-col gap-4'>
+        <h1 className='text-center mt-7 text-2xl font-semibold'>Your Listings</h1>
+        {userListings.map((listing) => (
+        <div key={listing._id} 
+             className='border rounded-lg p-3 flex justify-between items-center gap-4'>
+          <Link to={`/listing/${listing._id}`}>
+            <img src={listing.imageUrls[0]} 
+                 alt='listing cover' 
+                 className='h-16 w-16 object-contain'
+            />
+          </Link>
+          <Link className='text-slate-700 font-semibold flex-1 hover:underline truncate' to={`/listing/${listing._id}`}>
+            <p>{listing.name}</p>
+          </Link>
+          <div className='flex flex-col items-center'>
+            <button className='text-red-700 uppercase'>Delete</button>
+            <button className='text-green-700 uppercase'>Edit</button>
+  
+          </div>
+        </div>
+        ))}
+      </div>}
     </div>
   );
 }
-
-
