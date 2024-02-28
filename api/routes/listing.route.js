7778a593fe60aaf96a9222eb59ecdec6f009716b
shamim159 <shamin_ali@hotmail.co.uk>
2024-02-28 18:17:55 +0000

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -8,4 +8,6 @@ const router = express.Router();
 
 router.post('/create',verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
+
 export default router;
