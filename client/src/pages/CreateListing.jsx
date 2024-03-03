30e52c9f1a61675492763c77c5473e43a2268186
shamim159 <shamin_ali@hotmail.co.uk>
2024-03-03 22:56:02 +0000

    complete listing page

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -274,7 +274,9 @@ export default function CreateListing() {
                 />
                 <div className='flex flex-col items-center'>
                 <p>Regular Price</p>
-                <span className='text-xs'>(£ / month)</span>
+                {formData.type === 'rent' && (
+                  <span className='text-xs'>($ / month)</span>
+                )}
                 </div>
             </div>
             {formData.offer && (
@@ -291,8 +293,9 @@ export default function CreateListing() {
                />
                <div className='flex flex-col items-center'>
                <p>Discounted Price</p>
-               <span className='text-xs'>(£ / month)</span>
-               </div>
+               {formData.type === 'rent' && (
+                    <span className='text-xs'>($ / month)</span>
+                  )}               </div>
           </div>
           )}
         </div>
