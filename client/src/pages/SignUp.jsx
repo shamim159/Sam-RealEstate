ffd97bdbb7f5fe4e9f7e1182abec5e4c27251672
shamim159 <shamin_ali@hotmail.co.uk>
2024-03-07 22:11:20 +0000

    corrected password id

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -44,8 +44,8 @@ export default function SignUp() {
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
         <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
-        <input type="text" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-        <input type="text" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
+        <input type="password" placeholder='password' className='border p-3 rounded-lg' id='password' onChange={handleChange}/>
         <button disabled={loading} className=' bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>{loading ? 'Loading...' : 'Sign Up'}</button>
         <OAuth/>
       </form>
